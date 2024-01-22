83c80ac1bb935b4fbdb6a2f077257144e8c866c7
quickjkee <jkee2013@mail.ru>
2024-01-22 06:18:27 +0300

    yt specific

--- generate.py
+++ generate.py
@@ -1,4 +1,6 @@
 # make sure you're logged in with \`huggingface-cli login\`
+import copy
+
 from diffusers import StableDiffusionPipeline, StableDiffusionXLPipeline
 from diffusers import DDIMScheduler, DDPMScheduler, HeunDiscreteScheduler, EulerDiscreteScheduler
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
@@ -14,8 +16,22 @@ from nirvana_utils import copy_out_to_snapshot
 import time
 import cv2
 from PIL import Image
-from models import MODELS
 
+from utils import get_prompts, get_t2i_model
+from models import MODELS
+import yt.wrapper as yt
+from yt.wrapper import YtClient
+
+def image2bytes(
+    image, img_ext: str = ".png"
+) -> bytes:
+    """
+    image: C x H x W
+    """
+    image = np.array(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    _, image = cv2.imencode(img_ext, image)
+    return image.tobytes()
 
 parser = argparse.ArgumentParser(description='Generate images with stable diffusion')
 parser.add_argument('--steps', type=int, default=50, help='number of inference steps during sampling')
@@ -43,28 +59,14 @@ for k, v in sorted(vars(args).items()):
     dist.print0('\t{}: {}'.format(k, v))
 
 ###################
-# Prepare prompts #
+# Prepare prompts and model
 ###################
-if args.dataset == 'coco':
-    df = pd.read_csv('./prompts/coco.csv')
-    all_text = list(df['caption'])
-elif args.dataset == 'laion':
-    df = pd.read_csv('./prompts/laion.csv')
-    all_text = list(df['caption'])
-elif args.dataset == 'ir':
-    df = pd.read_csv('./prompts/image_reward_db.csv')
-    all_text = list(df['caption'])
-    assert len(all_text) == args.max_cnt == 463
-elif args.dataset == 'yagen':
-    df = pd.read_csv('./prompts/yagen_test.csv')
-    all_text = list(df['prompt_en'])
-    assert len(all_text) == args.max_cnt == 300
-elif args.dataset == 'custom':
-    df = pd.read_csv('./prompts/custom.tsv', sep='\t')
-    all_text = list(df['prompt'])
-    assert len(all_text) == 6234
 
+all_text = get_prompts(args)
 all_text = all_text[: args.max_cnt]
+pipe, refiner = get_t2i_model(args)
+
+# Splitting
 num_batches = ((len(all_text) - 1) // (args.bs * dist.get_world_size()) + 1) * dist.get_world_size()
 all_batches = np.array_split(np.array(all_text), num_batches)
 rank_batches = all_batches[dist.get_rank():: dist.get_world_size()]
@@ -73,57 +75,7 @@ index_list = np.arange(len(all_text))
 all_batches_index = np.array_split(index_list, num_batches)
 rank_batches_index = all_batches_index[dist.get_rank():: dist.get_world_size()]
 
-##################
-# Load T2I model #
-##################
-
-refiner = None
-if args.model == 'xl':
-    pipe = MODELS["stabilityai/stable-diffusion-xl-base-1.0"](
-        local_path=args.local_model_path
-    )
-elif args.model == 'xl_refiner':
-    pipe, refiner = MODELS["stabilityai/stable-diffusion-xl-refiner-1.0"](
-        local_path=args.local_model_path,
-        local_refiner_path=args.local_refiner_path,
-        use_refiner=True
-    )
-elif args.model == 'v2-1':
-    pipe = MODELS["stabilityai/stable-diffusion-2-1"](
-        local_path=args.local_model_path
-    )
-elif args.model == 'v1-5':
-    pipe = MODELS["runwayml/stable-diffusion-v1-5"](
-        local_path=args.local_model_path
-    )
-else:
-    raise Exception(f"Not found {args.model}")
-
-dist.print0("Default scheduler:")
-dist.print0(pipe.scheduler)
-
-##########################
-# Setup custom scheduler #
-##########################
-
-if args.scheduler:
-    if args.scheduler == 'DDPM':
-        pipe.scheduler = DDPMScheduler.from_config(pipe.scheduler.config)
-    elif args.scheduler == 'DDIM':
-        pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
-    elif args.scheduler == 'Heun':
-        pipe.scheduler = HeunDiscreteScheduler.from_config(pipe.scheduler.config)
-    elif args.scheduler == 'DPMSolver':
-        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    elif args.scheduler == 'ODE':
-        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
-        pipe.scheduler.use_karras_sigmas = False
-    else:
-        raise NotImplementedError
-
-    dist.print0("New scheduler:")
-    dist.print0(pipe.scheduler)
-
+# Seeds
 if dist.get_rank() == 0:
     t0 = time.time()
 
@@ -136,55 +88,38 @@ else:
 # Setup custom scheduler #
 ##########################
 
-for seed in seeds:
-    generator = torch.Generator(device="cuda").manual_seed(seed)
-    if refiner is not None:
-        refiner_generator = torch.Generator(device="cuda").manual_seed(seed)
+## generate images ##
+client = YtClient(proxy="hahn")
+path = yt.TablePath("//home/yr/quickjkee/sdxl_coco", append=True, client=client)
+row = [{'prompt': None, 'seeds': None, 'model': None,
+         'image_1': None, 'image_2': None, 'image_3': None, 'image_4': None, 'image_5': None,
+         'image_6': None, 'image_7': None, 'image_8': None, 'image_9': None, 'image_10': None}
+        ]
 
-    ##### setup save configuration #######
-    if args.name is None:
-        save_dir = os.path.join(args.save_path,
-                                f'steps_{args.steps}_w_{args.w}_seed_{seed}')
-    else:
-        save_dir = os.path.join(args.save_path,
-                                f'steps_{args.steps}_w_{args.w}_seed_{seed}_name_{args.name}')
+for cnt, mini_batch in enumerate(tqdm.tqdm(rank_batches, unit='batch', disable=(dist.get_rank() != 0))):
+    text = list(mini_batch)
+    new_row = copy.deepcopy(row)
+    new_row[0]['prompt'] = text[0]
+    new_row[0]['model'] = args.name
 
-    dist.print0("save images to {}".format(save_dir))
-
-    if dist.get_rank() == 0:
-        os.makedirs(args.save_path, exist_ok=True)
-        os.makedirs(save_dir, exist_ok=True)
-
-    ## generate images ##
-    for cnt, mini_batch in enumerate(tqdm.tqdm(rank_batches, unit='batch', disable=(dist.get_rank() != 0))):
-        text = list(mini_batch)
-        images = pipe(
-            text, 
-            generator=generator, 
-            num_inference_steps=args.steps, 
+    for it, seed in enumerate(range(cnt * 10, cnt * 10 + 10)):
+        new_row[0]['seeds'] = [list(range(cnt * 10, cnt * 10 + 10))]
+        generator = torch.Generator().manual_seed(seed)
+        image = pipe(
+            text,
+            generator=generator,
+            num_inference_steps=args.steps,
             guidance_scale=args.w,
-        ).images
+        ).images[0]
+        image = image2bytes(image)
+        new_row[0][f'{image}_{it+1}'] = image
 
-        if refiner is not None:
-            images = refiner(
-                prompt=text, 
-                negative_prompt=len(text)*[""],
-                generator=refiner_generator,
-                image=images
-            ).images
+    client.write_table(path, new_row, raw=False)
 
-        for text_idx, global_idx in enumerate(rank_batches_index[cnt]):
-            if args.resize_output is not None:
-                images[text_idx] = Image.fromarray(cv2.resize(
-                     np.array(images[text_idx]), 
-                    (args.resize_output, args.resize_output),
-                    interpolation=cv2.INTER_AREA
-                ))
-            images[text_idx].save(os.path.join(save_dir, f'{global_idx}.jpg'))
 # Done.
 dist.barrier()
 if dist.get_rank() == 0:
-    print(f"Overall time: {time.time()-t0:.3f}")
+    print(f"Overall time: {time.time() - t0:.3f}")
     d = {'caption': all_text}
     df = pd.DataFrame(data=d)
     df.to_csv(os.path.join(args.save_path, 'generated_prompts.csv'))
