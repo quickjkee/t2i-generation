bcdb227f1f0df8b8392a1cf9d557d6b7dfc674ff
quickjkee <jkee2013@mail.ru>
2024-01-25 06:34:49 +0300

    yt specific

--- utils.py
+++ utils.py
@@ -28,7 +28,6 @@ def get_prompts(args):
     elif args.dataset == 'pickscore':
         df = pd.read_csv('./prompts/pickscore_40k.csv')
         all_text = list(df['caption'])
-        assert len(all_text) == args.max_cnt == 300
     elif args.dataset == 'custom':
         df = pd.read_csv('./prompts/custom.tsv', sep='\t')
         all_text = list(df['prompt'])
